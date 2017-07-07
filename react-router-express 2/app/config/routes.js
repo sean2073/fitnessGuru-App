7c5ee1813ca02d5e0164d7dca61e3c989fc73a7b
Patrick316 <pattumulty@gmail.com>
2017-07-06 23:55:26 -0400

    route changes

--- react-router-express 2/app/config/routes.js
+++ react-router-express 2/app/config/routes.js
@@ -1,22 +1,9 @@
-// Include the React library
-var React = require("react");
-
-// Include the react-router module
-var router = require("react-router");
-
-// Include the Route component for displaying individual routes
-var Route = router.Route;
-
-// Include the Router component to contain all our Routes
-// Here where we can pass in some configuration as props
-var Router = router.Router;
-
-// Include the hashHistory prop to handle routing client side without a server
-// https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
-var hashHistory = router.hashHistory;
-
-// Include the IndexRoute (catch-all route)
-var IndexRoute = router.IndexRoute;
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
 
 // Reference the high-level components
 var Profile = require("../components/Profile");
@@ -33,19 +20,18 @@ module.exports = (
 
   // The high level component is the Router component
   <Router history={hashHistory}>
-    <Route path="/" component={Profile}>
+    <Route path="/" component={Create}>
 
       {/* If user selects Info or Chat show the appropriate component */}
       <Route path="dashboard" component={Dashboard} />
       <Route path="goals" component={Goals} />
       <Route path="Login" component={Login} />
       <Route path="goals" component={Goals} />
-      <Route path="Create" component={Create} />
+      <Route path="profile" component={Profile} />
       <Route path="Success" component={Success} />
       {/* If user selects any other path... we get the Info Route */}
       <IndexRoute component={Info} />
 
     </Route>
   </Router>
-
 );
