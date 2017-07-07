2503ce11cde015468dd750546281c1a3521c3267
Patrick316 <pattumulty@gmail.com>
2017-07-07 00:00:10 -0400

    helpers.js  in the utility folder

---
+++ react-router-express 2/app/utility/helpers.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+
+
+  export const getData = () => {
+
+      return axios.get('http://localhost:3001/create')
+        .then((data,err)=>{
+          if(!err) return data;
+
+          console.log(`
+              This is the data from the db
+              -------------------------------------
+              ${data}
+            `)
+        })
+
+  }
+
+	export const postData = (username,password)=>{
+		const posts = {
+      username:username,
+      password:password
+    }
+		return axios.post('http://localhost:3001/create', posts).then((response)=>{
+			console.log("new response:", response);
+		}).catch((err)=>{
+			console.log("New error:", err)
+		});
+
+	}
